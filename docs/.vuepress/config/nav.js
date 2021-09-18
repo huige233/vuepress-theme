16feebec36be5ab89a6f4048b6b33ab47fdb8e29
huige233 <huige1463@gmail.com>
2021-09-18 15:10:44 +0800

    1

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -3,7 +3,15 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: '编程内容',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/web/', 
+    items: [
+      {text: '技术文档', link: '/web/#技术文档'},
+      {text: 'CSS', link: '/web/#CSS'},
+      {text: 'JavaScripts', link: '/web/#JavaScript文章'},
+      {text: 'other', link: '/web/#other'},
+      {text: '学习笔记', link: '/web/#学习笔记'},
+    ]
+    //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
   {
     text: '更多',
@@ -14,17 +22,12 @@ module.exports = [
       { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
       { text: '实用技巧', link: '/pages/baaa02/' },
       { text: '友情链接', link: '/friends/' },
+      {text: '收藏',link: '/pages/beb6c0bd8a66cea6/'},
+      { text: '关于', link: '/about/' },
+      {text: '肯定有帮助', link: 'https://baidu.com'},
+      {text: 'CSDN', link: 'https://blog.csdn.net/'},
+      {text: 'MyGithub', link: 'https://github.com/huige233'}	  
     ],
-  },
-  { text: '关于', link: '/about/' },
-  {
-    text: '收藏',
-    link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
   },
   {
     text: '索引',
@@ -35,7 +38,4 @@ module.exports = [
       { text: '归档', link: '/archives/' },
     ],
   },
-  {text: '肯定有帮助', link: 'https://baidu.com'},
-  {text: 'CSDN', link: 'https://blog.csdn.net/'},
-  {text: 'MyGithub', link: 'https://github.com/huige233'}	  
 ]
