8884b785913a17f0221e1a4ce74f6eb4ac859748
huige233 <huige1463@gmail.com>
2021-10-12 16:23:57 +0800

    新章节

---
+++ docs/01.编程内容/15.C#/08-面向对象/03-静态和非静态的区别/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_静态和非静态的区别
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //调用实例成员
+            Person p = new Person();
+            p.M1();
+            Person.M2();
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ docs/01.编程内容/15.C#/08-面向对象/05-练习/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t = new Ticket(350);
+            t.ShowTicket();
+            Console.ReadKey();
+
+        }
+    }
+}
