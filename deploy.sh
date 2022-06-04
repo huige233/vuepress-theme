6eada3c92e36da7664ca00ee40e158a7d712f81c
lijiakaijun <wwln@lijiakaijun.cyou>
2022-06-05 03:35:10 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ git init
 git branch -m master gh-pages
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl origin gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
