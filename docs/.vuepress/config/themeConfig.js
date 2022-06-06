90d1d57a5cf286c3ffcbeed5e4735dbfed18f77a
huige <61108540+huige233@users.noreply.github.com>
2022-06-06 14:14:41 +0800

    fix a bug

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -11,6 +11,7 @@ module.exports = {
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
+    docsBranch: 'main', // 编辑的分支
     editLinks: true, // 启用编辑
     editLinkText: '编辑',
 
