46cab2dc1347690553bf85c800d8ff35fa4a9591
huige233 <huige1463@gmail.com>
2022-06-29 18:42:15 +0800

    2022.6.29

--- deplo.cmd
+++ deplo.cmd
@@ -1,11 +1,5 @@
-
-pause
 cd .\docs\.vuepress\dist
-pause
 git init
-pause
 git add -A
-pause
 git commit -m "commit in myself"
-pause
 git push -f git@github.com:huige233/vuepress-theme.git master:gh-pages
